c0817a9c04e237219a51a54d85d72ade4a9d94ba
xingjian <xili9453@uni.sydney.edu.au>
2022-08-22 08:59:34 +1000

    update

---
+++ src/10_组件状态的定义使用.js
@@ -0,0 +1,67 @@
+import React from "react";
+// 组件状态流程：初始化状态-读取状态-修改抓过你太-影响视图
+// 现在：类组件
+
+// 首先把TestComponent的架子搭出来，然后再思考如何定义状态
+// class TestComponent extends React.Component{
+//     render(){
+//         return <div>this is a TestComponent</div>
+//     }
+// }
+
+// 定义状态，使用状态
+// class TestComponent extends React.Component{
+//     // 1. 定义组件状态
+//     // 写个绝对属性就行
+//     state = {
+//         // 用对象的语法，定义各种各样属性，全都是当前组件的状态
+//         name: 'lydia'
+//     }
+//     render(){
+//         //2. 使用状态
+//         return (<div>
+//             this is a TestComponent
+//             当前name为：{this.state.name}
+//         </div>)
+//     }
+// }
+
+// 修改状态：点击事件触发修改
+class TestComponent extends React.Component{
+    // 1. 定义组件状态
+    // 写个绝对属性就行
+    state = {
+        // 用对象的语法，定义各种各样属性，全都是当前组件的状态
+        name: 'lydia'
+    }
+    // 定义事件回调函数
+    changeName = () =>{
+        // 3. 修改state的状态name
+        // 不是this.state.name = 新name
+        // 必须用setState
+        this.setState({
+            name: '李行健'
+        })
+    }
+
+    render(){
+        //2. 使用状态
+        return (<div>
+            this is a TestComponent
+            当前name为：{this.state.name}
+            <button onClick={this.changeName}>修改name</button>
+        </div>)
+    }
+}
+
+
+function App() {
+    return (
+        <div className="App">
+            <TestComponent>
+
+            </TestComponent>
+        </div>
+    )
+}
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React from "react";
 
-
-function Hello(){
-    const clickHandler = (e,msg) =>{
-
-        console.log('函数组件的事件被触发了',msg,e)
-    }
-
-    return <div onClick={(e)=>clickHandler(e,'this is massage')}>点我点我</div>
-}
-class HelloComponent extends React.Component{
-    clickHandler = () =>{
-        console.log('类组件的事件被触发了')
-    }
-    render(){
-        return <div onClick={this.clickHandler}>this is component</div>
-    }
-}
 function App() {
     return (
         <div className="App">
-            <Hello></Hello>
-            {/* 渲染函数组件 */}
-            <Hello/>
-            {/*渲染类组件*/}
-            <HelloComponent></HelloComponent>
-
 
         </div>
     )
