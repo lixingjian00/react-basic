ad4d987a765c665f3c9eaf805c12114e2144d1e0
lixingjian00 <lixingjian00@gmail.com>
2022-08-23 16:46:30 +1000

    update

--- src/13_表单受控组件.js
+++ src/13_表单受控组件.js
@@ -3,11 +3,36 @@ import React from "react"
 // 受控组件
 // input// input的状态被React组件的状态控制的状态被React组件（state）的状态控制
 
+// 1. 声明用来控制input value状态的   react组件自己的状态
+class Counter extends React.Component {
+    state = {
+        message: 'this is message'
+    }
+    inputChange = (e) =>{
+        // 3. 看看是否被触发，并打印一下事件对象e e里面有target的value值（就是'this is message'）
+        console.log('change触发了', e)
+        // 4. 拿到输入框最新的值，也就是e里面的target.value 交给state中的message
+        this.setState({
+            message: e.target.value
+        })
+    }
+    render () {
+        return (
+            // 2. 给input框的value属性，绑定react state
+            <input type="text"
+                   value={this.state.message}
+                // 框里输入东西的时候，会触发change事件
+                // 3. 给input框绑定一个change事件，为了拿到当前输入框中的数据
+                   onChange={this.inputChange}
+            />
+        )
+    }
+}
 
 function App() {
     return (
         <div className="App">
-
+            <Counter></Counter>
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -1,65 +1,31 @@
-import React from "react"
+import React, {createRef} from "react"
 
-class TestComponent extends React.Component{
-
-    state = {
-        // 用对象的语法，定义各种各样属性，全都是当前组件的状态
-        name: 'lydia',
-        list: [1, 2, 3],
-        person: {
-            name: 'jack',
-            age: 20
-        }
+// 非受控组件
+// 手动操作dom的方式获取文本框的值，状态不受react组件控制，通过原生dom获取
+// 1. 导入creatRef，相当于创建一个容器，dom可以往里放
+// 2. 创建ref对象后，存到msgRef的实例属性中
+// 3. 为input添加red属性，值为msgRef
+// 4. 事件处理的时候，通过msg.current可以拿到input对应的dom元素，msgRef.current.value就是文本框的值
+class Counter extends React.Component {
+    msgRef = createRef() // 容器
+    getValue = () => {
+        // 通过msgRef获取input value的值
+        console.log(this.msgRef.current.value)
     }
-
-    change () {
-
-
-        this.setState({
-            name: '李行健'
-        })
-        this.setState({
-            list: [...this.state.list, 4]
-        })
-        this.setState({
-            person: {
-                ...this.state.person,
-                name: 'rose'
-            }
-        })
-        // list删除
-        this.setState({
-            list: this.state.list.filter(item => item !== 2)
-        })
-    }
-
-    render(){
-        //2. 使用状态
+    render () {
         return (
             <>
-                <div>{this.state.name}</div>
-                <ul>
-                    {this.state.list.map( (item) => <li key={item}>{item}</li> )}
-                </ul>
-                <div>
-                    {this.state.person.name}
-                </div>
-                <div>
-                    <button onClick={() => this.change()}>修改list和name</button>
-                </div>
+                <input type="text" ref={this.msgRef}/>
+                <button onClick={this.getValue}>点击获取输入框的值</button>
             </>
-
         )
     }
 }
 
-
 function App() {
     return (
         <div className="App">
-            <TestComponent>
-
-            </TestComponent>
+            <Counter></Counter>
         </div>
     )
 }
