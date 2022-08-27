0a7aeea5aec3c2b5ae3b3c7ffe4653eb13b80448
lixingjian00 <lixingjian00@gmail.com>
2022-08-27 16:22:48 +1000

    propType

--- src/20_propType.js
+++ src/20_propType.js
@@ -6,13 +6,27 @@ import PropTypes from 'prop-types'
 // 2. 导入prop-types 包
 // 3. 使用 组件名.propTypes = {} 给组件添加校验规则
 
-function Test ({list}) {
+function Test ({list, pageSize=10}) {
+    // 这里设置函数组件默认值
     return <div>
         {list.map(item => <p key={item}>{item}</p>)}
+        {pageSize}
     </div>
 }
+class TestC extends React.Component{
+    static defaultProps = {
+        pageSize: 10
+    }
+    render() {
+        return(
+            <div>{this.props.pageSize}</div>
+        )
+    }
+
+}
 Test.propTypes = {
-    list: PropTypes.array // 限定这里list参数的类型必须是数组类型
+    list: PropTypes.array.isRequired // 限定这里list参数的类型必须是数组类型
+    // .isRequired 如果App没传，就会报警告
 }
 
 class App extends React.Component{
@@ -20,7 +34,10 @@ class App extends React.Component{
     render(){
         return (
             // <Test list={9}></Test> // 这样会报错
-            <Test list={[1,2,3]}></Test>
+            <>
+                <Test list={[1,2,3]} pageSize={20}></Test>
+                <TestC pageSize={20}></TestC>
+            </>
         )
     }
 }
